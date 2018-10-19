87f08d4f4287fa5a7483594ffb9c26b72ab3050c
Jonathan Ngondi Munyi <jnmunyi@safaricom.co.ke>
2018-10-19 18:11:49 +0300

    [Tests]Adds tests for the ViewModel class

--- app/src/test/java/com/mugiwara/savingcalculator/viewmodel/SavingsCalculatorTest.kt
+++ app/src/test/java/com/mugiwara/savingcalculator/viewmodel/SavingsCalculatorTest.kt
@@ -1,2 +1,99 @@
 package com.mugiwara.savingcalculator.viewmodel
 
+import com.mugiwara.savingcalculator.calculator.viewmodel.CalculatorViewModel
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.Before
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class SavingsCalculatorTest {
+
+  private lateinit var calculatorViewModel : CalculatorViewModel
+
+  @Before
+  fun setUp(){
+    calculatorViewModel = CalculatorViewModel()
+  }
+
+  @Test
+  fun check_algorithm_for_positive_integers() {
+
+    //Given
+    val startingAmount: Int = 20
+    val difference: Int = 20
+    //Then
+    assertEquals(27560, calculatorViewModel.calculateTotal(startingAmount,difference))
+  }
+
+  @Test
+  fun check_algorithm_for_negative_starting_amount() {
+
+    //Given
+    val startingAmount: Int = -20
+    val difference: Int = 20
+    //Then
+    assertEquals(25480, calculatorViewModel.calculateTotal(startingAmount,difference))
+  }
+
+  @Test
+  fun check_algorithm_for_negative_difference() {
+
+    //Given
+    val startingAmount: Int = 20
+    val difference: Int = -20
+    //Then
+    assertEquals(0, calculatorViewModel.calculateTotal(startingAmount,difference))
+  }
+
+  @Test
+  fun check_algorithm_for_period_less_than_a_year(){
+    //Given
+    val startingAmount: Int = 20
+    val difference: Int = 20
+    val period = 24.toFloat()
+
+    //Then
+    assertEquals(6000, calculatorViewModel.calculateTotalForPeriod(startingAmount,difference, period))
+  }
+
+  @Test
+  fun check_algorithm_for_period_that_is_odd(){
+    //Given
+    val startingAmount: Int = 20
+    val difference: Int = 20
+    val period = 5.toFloat()
+
+    //Then
+    assertEquals(300, calculatorViewModel.calculateTotalForPeriod(startingAmount,difference, period))
+  }
+
+  @Test
+  fun get_number_of_weeks_in_a_period(){
+    //Given
+    val weeksInAPeriod = TestDataStore.tenWeeks()
+
+    //Then
+    assertEquals(weeksInAPeriod, calculatorViewModel.getTheNumberOfWeeksInAPeriod(10))
+
+  }
+
+  @Test
+  fun calculation_for_arbitrary_period(){
+    //Given
+    val period = 10
+    val savingsAmount = 50
+
+    //When
+    val tenWeeksofSavings = TestDataStore.tenWeeksofSavings()
+
+    //Then
+    assertEquals(tenWeeksofSavings, calculatorViewModel.getTheTotalSavingsForAPeriod(period,savingsAmount))
+  }
+
+
+}
--- app/src/test/java/com/mugiwara/savingcalculator/viewmodel/TestDataStore.kt
+++ app/src/test/java/com/mugiwara/savingcalculator/viewmodel/TestDataStore.kt
@@ -1,4 +1,17 @@
 package com.mugiwara.savingcalculator.viewmodel
 
 object TestDataStore {
+
+  fun fiveWeeks() :List<Int>{
+
+    return listOf(1,2,3,4,5)
+  }
+
+  fun tenWeeks() :List<Int>{
+    return listOf(1,2,3,4,5,6,7,8,9,10)
+  }
+
+  fun tenWeeksofSavings(): List<Int>{
+    return listOf(50,150,300,500,750,1050,1400,1800,2250,2750)
+  }
 }
