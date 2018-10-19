7c3e6fd1ddc3af2dbdb4e749433e96d124713b1f
Jonathan Ngondi Munyi <jnmunyi@safaricom.co.ke>
2018-10-19 18:13:23 +0300

    [Feature]Added functionality and setup of LiveData object

--- app/src/main/java/com/mugiwara/savingcalculator/calculator/viewmodel/CalculatorViewModel.kt
+++ app/src/main/java/com/mugiwara/savingcalculator/calculator/viewmodel/CalculatorViewModel.kt
@@ -1,4 +1,61 @@
 package com.mugiwara.savingcalculator.calculator.viewmodel
 
-class CalculatorViewModel {
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
+import com.mugiwara.savingcalculator.calculator.data.PeriodRepository
+import com.mugiwara.savingcalculator.calculator.data.SavingsClass
+
+class CalculatorViewModel : ViewModel() {
+
+  val total = MutableLiveData<Int>()
+
+
+  fun getTheTotalSavingsForAPeriod(weeks:Int, startingAmount:Int) : List<Int>{
+    val totals = mutableListOf<Int>()
+    for(i in getTheNumberOfWeeksInAPeriod(weeks)){
+      totals.add(calculateTotalForPeriod(startingAmount,startingAmount, i.toFloat()))
+    }
+    return totals
+  }
+
+
+  fun getTheNumberOfWeeksInAPeriod(weeks:Int) : List<Int> {
+    val periodRepository = PeriodRepository()
+    return periodRepository.getNumberOfWeeksInAPeriod(weeks)
+  }
+
+  fun calculateTotal(startingAmount: Int, difference: Int) : Int {
+    val numberOfWeeks = 52
+    var totalInt = 0
+    if(difference<0){
+      totalInt = 0
+      total.value = totalInt
+
+    } else {
+      totalInt = numberOfWeeks / 2 * (2*(startingAmount)+(numberOfWeeks-1)*difference)
+      total.value = totalInt
+
+    }
+    return totalInt
+
+  }
+
+  fun calculateTotalForPeriod(startingAmount: Int, difference: Int, period: Float): Int {
+    return ((2*(startingAmount)+(period-1)*(difference))*(period/2)).toInt()
+  }
+
+  fun saveListOfSavings(startingAmount: Int, period: Int, periodRepository: PeriodRepository){
+    val totals = getTheTotalSavingsForAPeriod(period,startingAmount)
+    periodRepository.saveData(totals)
+  }
+
+  fun getSavedTotals(periodRepository: PeriodRepository):MutableLiveData<SavingsClass>{
+    return periodRepository.getData()
+  }
+
+
+
+
+
+
 }
